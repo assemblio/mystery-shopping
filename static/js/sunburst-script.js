7641da7304af4067852db7bba1de9bebc8cfa10c
Vullkan92 <vullkan.halili@riinvest.net>
2016-11-24 23:15:23 +0100

    minor changes

--- static/js/sunburst-script.js
+++ static/js/sunburst-script.js
@@ -701,16 +701,23 @@ suffix_choices = {
     "A3": "minutes",
     "A4": "times",
     "P4": "€"
-}
+};
 
 window.suffix = "";
 
 $(function () {
+    // draw sequences sunburst chart on page load.
     var indicator = $("#indicator-select").val();
     window.suffix = suffix_choices[indicator];
     drawSequencesChart(data[indicator]);
     var selected_question_text = $("#indicator-select option:selected").text();
     $(".question-label").empty().text(selected_question_text);
+
+    // add class active to the aggregated optin in the navbar
+    $(".left-nav li:first").removeClass("active");
+    $(".left-nav li:last").addClass("active");
+
+    // indicator select change.
     $("#indicator-select").on("change", function () {
         var selected_question_text = $("#indicator-select option:selected").text();
         $(".question-label").empty().text(selected_question_text);
